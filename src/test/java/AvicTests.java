6ea2f8d974ea01628ac9dd33db799bcf3a8bbd66
tXlexGPA <txlexgpa@email.com>
2021-12-21 17:30:12 +0200

    new xpath

--- src/test/java/AvicTests.java
+++ src/test/java/AvicTests.java
@@ -38,16 +38,16 @@ public class AvicTests {
     public void checkChat(){
         String INPUT_TEXT = "Hello";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        driver.findElement(xpath("//jdiv [@class='hoverl_bca9']")).click();
-        driver.findElement(xpath("//textarea [@class='inputField_0bb0']"))
+        driver.findElement(xpath("//*[@id='jvlabelWrap']/jdiv[@class='hoverl_72cd']")).click();
+        driver.findElement(xpath("//textarea[@class='inputField_adc3']"))
                 .sendKeys( INPUT_TEXT, Keys.ENTER);
-        assertTrue(driver.findElement(xpath("//jdiv [contains(@class,'client')]/jdiv [contains(@class,'text')]")).getText().contains( INPUT_TEXT));
+        assertTrue(driver.findElement(xpath("//jdiv[contains(@class,'message')]/jdiv[contains(@class,'text')]")).getText().contains( INPUT_TEXT));
     }
 
     @Test(priority = 3)
     public void checkButtonMi() {
         driver.findElement(xpath("//img [contains(@src,'mi-brand')]")).click();
-        new WebDriverWait(driver, 30).until(
+        new WebDriverWait(driver, 2).until(
                 webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));
         List<WebElement> elementsList = driver.findElements(xpath("//article [@class='brand__item']"));
         int actualElementsSize = elementsList.size();
@@ -60,16 +60,16 @@ public class AvicTests {
         driver.findElement(xpath("//span[@class='sidebar-item']")).click();
         driver.findElement(xpath("//ul[contains(@class,'sidebar-list')]//a[contains(@href,'telefonyi-i-aksessuaryi')]")).click();
         driver.findElement(xpath("//div/a[@href='https://avic.ua/smartfonyi']")).click();
-        new WebDriverWait(driver, 30).until(
+        new WebDriverWait(driver, 2).until(
                 webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));
-        driver.findElement(xpath("//a [contains(@data-ecomm-cart, 'Xiaomi Poco X3 Pro 6\\/128GB Frost Blue')]")).click();
+        driver.findElement(xpath("//a [contains(@data-ecomm-cart, 'Смартфон Xiaomi Poco X3 Pro 8\\/256GB Frost Blue')]")).click();
         WebDriverWait wait = new WebDriverWait(driver, 30);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("js_cart")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='js_cart']")));//*
         driver.findElement(xpath("//a [text()='Оформить заказ']")).click();
-        new WebDriverWait(driver, 30).until(
+        new WebDriverWait(driver, 2).until(
                 webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));
         boolean actualProductsCountInCart =
-                driver.findElement(xpath("//span [contains(text(),'Xiaomi Poco X3 Pro 6/128GB Frost Blue')]"))
+                driver.findElement(xpath("//span [contains(text(),'Смартфон Xiaomi Poco X3 Pro 8/256GB Frost Blue')]"))
                         .getText().isEmpty();
         assertFalse(actualProductsCountInCart);
     }
@@ -81,10 +81,10 @@ public class AvicTests {
         driver.findElement(xpath("//div/a[@href='https://avic.ua/smartfonyi']")).click();
         new WebDriverWait(driver, 30).until(
                 webDriver -> ((JavascriptExecutor) webDriver).executeScript("return document.readyState").equals("complete"));
-        driver.findElement(xpath("//a [@title='Смартфон Xiaomi Poco X3 Pro 6/128GB Metal Bronze']")).click();
-        WebDriverWait wait = new WebDriverWait(driver, 30);//ждем пока не отобразится попап с товаром добавленным в корзину
-            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//img [contains(@src, 'smartfon-xiaomi-poco-x3-pro-6128gb-metal-bronze-2-prod_sm')]")));
-        boolean actualValue = driver.findElement(By.xpath("//img [contains(@src, 'smartfon-xiaomi-poco-x3-pro-6128gb-metal-bronze-2-prod_sm')]")).isEnabled();
+        driver.findElement(xpath("//a[contains(@href,'xiaomi-poco-x3-pro-8256gb-metal-bronze')]")).click();
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//img[contains(@src,'xiaomi-poco-x3-pro-6128gb-metal-bronze')]")));
+        boolean actualValue = driver.findElement(By.xpath("//img[contains(@src,'xiaomi-poco-x3-pro-6128gb-metal-bronze')]")).isEnabled();
         assertTrue(actualValue);
     }
 
